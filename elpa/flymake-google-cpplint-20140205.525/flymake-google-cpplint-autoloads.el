5731f6738bd183ca70b6a77a165a61960a9b981f
TakayamaNatsuki <takayaman@TakayamaNatsuki-no-MacBook-Air.local>
2014-10-29 22:09:26 +0900

    elpaフォルダ追加

---
+++ elpa/flymake-google-cpplint-20140205.525/flymake-google-cpplint-autoloads.el
@@ -0,0 +1,29 @@
+;;; flymake-google-cpplint-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (flymake-google-cpplint-load) "flymake-google-cpplint"
+;;;;;;  "flymake-google-cpplint.el" (21570 7639 6607 85000))
+;;; Generated autoloads from flymake-google-cpplint.el
+
+(autoload 'flymake-google-cpplint-load "flymake-google-cpplint" "\
+Configure flymake mode to check the current buffer's C/C++ source.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("flymake-google-cpplint-pkg.el") (21570
+;;;;;;  7639 89654 623000))
+
+;;;***
+
+(provide 'flymake-google-cpplint-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-google-cpplint-autoloads.el ends here
---
+++ elpa/wgrep-2.1.3/wgrep-autoloads.el
@@ -0,0 +1,29 @@
+;;; wgrep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (wgrep-setup) "wgrep" "wgrep.el" (20924 8314 92820
+;;;;;;  300000))
+;;; Generated autoloads from wgrep.el
+
+(autoload 'wgrep-setup "wgrep" "\
+Setup wgrep preparation.
+
+\(fn)" nil nil)
+(add-hook 'grep-setup-hook 'wgrep-setup)
+
+;;;***
+
+;;;### (autoloads nil nil ("wgrep-pkg.el") (20924 8314 130702 942000))
+
+;;;***
+
+(provide 'wgrep-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; wgrep-autoloads.el ends here
