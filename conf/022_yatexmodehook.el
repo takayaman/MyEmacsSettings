96c479ad977d73c01600edac7e664a5a98bd2ad7
takayaman <takayaman@uec.ac.jp>
2014-11-01 21:05:49 +0900

    YaTeX導入+outline modeの設定

---
+++ conf/022_yatexmodehook.el
@@ -0,0 +1,60 @@
+;; Yatexモード設定ファイル
+; 拡張子とモードの結びつけ
+(setq auto-mode-alist 
+      (append '(
+	       ("\\.tex$" . yatex-mode)
+	       ) auto-mode-alist)
+)
+; 必要になったときに読み込む
+(autoload 'yatex-mode "yatex" "Yet Another Latex mode" t)
+;; LaTexコマンド，プレビューワ，プリンタなどの設定
+(setq tex-command "platex")
+; Linux, mac : pxdvi, windows or console : dviout
+(setq dvi2-command 
+      (cond ((eq window-system 'x)
+	     "pxdvi -expert -p 300 -s 3 -geo 750x575+0+0")
+	    ((eq window-system 'ns)
+	     "pxdvi -expert -p 300 -s 3 -geo 750x575+0+0")
+	    ((eq window-system 'w32)
+	     "dviout")
+	    (nil
+	     "dviout"))
+)
+(setq dviprint-command-format "dvipdfmx %s | lpr")
+; (nil ignore, 0 no convert, 1 JIS, 2 SJIS, 3 EUC, 4 UTF-8)
+(setq YaTeX-kanji-code nil)
+; AMS-LaTexを使う
+(setq YaTeX-user-AMS-LaTex t)
+(setq makeindex-command "mendex")
+
+;; 動作設定
+; 括弧入力時に閉じ括弧の同時挿入禁止
+(setq YaTeX-modify-mode t)
+(setq YaTeX-close-paren-always nil)
+
+; yatex outline-minor-mode
+(setq-default outline-level 'outline-level)
+(defun my:yatex-mode-hook ()
+  (make-variable-buffer-local 'outline-level)
+  (setq outline-level 'latex-outline-level)
+  (make-variable-buffer-local 'outline-regexp)
+  (setq outline-regexp
+	(concat "[ \t]*\\\\\\(documentstyle\\|documentclass\\|"
+                  "chapter\\|section\\|subsection\\|subsubsection\\)"
+                  "\\*?[ \t]*[[{]"))
+)
+
+(defun latex-outline-level ()
+  (save-excursion
+    (looking-at outline-regexp)
+    (let ((title (buffer-substring (match-beginning 1) (match-end 1))))
+      (cond ((eq (substring title 0 4) "docu") 20)
+	    ((eq (substring title 0 4) "para")
+	    ((eq (substring title 0 4) "chap") 0)
+	    ((eq (substring title 0 4) "appe") 0)
+	    (t (length title))))
+      )
+    )
+)
+(add-hook 'yatex-mode-hook 'my:yatex-mode-hook)
+(add-hook 'yatex-mode-hook '(lambda () (outline-minor-mode t)))
--- conf/02_modehookenv.el
+++ conf/02_modehookenv.el
@@ -1,5 +1,15 @@
 ;; Emacs環境設定 モード毎の設定
 
+; outline-mode全体設定
+(setq outline-minor-mode-prefix "\C-c\C-o")
+(eval-after-load 'outline
+  '(progn
+     (require 'outline-magic)
+     (define-key outline-minor-mode-map (kbd "<C-tab>") 'outline-cycle)
+     )
+  )
+
+
 ; elisp-------------------------------------------------
 (defun my:elisp-mode-hooks ()
   ; モードラインにelispシンボルの内容を表示
