ec417dae45ebb2ad35fab50d024f84cdc1b28ef0
takayaman <takayaman@uec.ac.jp>
2015-06-20 18:53:14 +0900

    Add settings for editting english documents.

--- conf/02_modehookenv.el
+++ conf/02_modehookenv.el
@@ -17,36 +17,6 @@
      )
   )
 
-; sdic 辞書設定--------------------------------------------
-(require 'sdic-inline)
-(sdic-inline-mode t)
-(setq sdic-inline-world-at-point-strinct t)
-; 辞書ファイル設定
-(cond ((eq system-type 'gnu/linux)
-       (setq sdic-inline-eiwa-dictionary "/usr/share/dict/gene.sdic")
-       (setq sdic-inline-waei-dictionary "/usr/share/dict/jedict.sdic")
-       ))
-; sdit tooltip
-(require 'sdic-inline-pos-tip)
-(setq sdic-inline-display-func 'sdic-inline-pos-tip-show)
-
-(setq transient-mark-mode t)
-(setq sdic-inline-search-func 'sdic-inline-search-word-with-stem)
-(setq sdic-inline-display-func 'sdic-inline-pos-tip-show-when-region-selected)
-; ONにする拡張子設定
-(setq sdic-inline-enable-filename-regex "\\.\\(txt\\|tex\\)$")
-(define-key sdic-inline-map "\C-c\C-p" 'sdic-inline-pos-tip-show)
-
-(defun sdic-inline-pos-tip-show-when-region-selected (entry)
-  (cond
-   ((and transient-mark-mode mark-active)
-    (funcall 'sdic-inline-pos-tip-show entry))
-   (t
-    ;;(funcall 'sdic-inline-display-minibuffer entry)
-    )
-   )
-  )
-
 ; elisp-------------------------------------------------
 (defun my:elisp-mode-hooks ()
   ; モードラインにelispシンボルの内容を表示
