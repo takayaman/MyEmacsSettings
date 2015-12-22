2fae7243556ba21273b2b4d8374207181ffbfdcc
takayaman <takayaman@uec.ac.jp>
2015-12-22 13:31:08 +0900

    Add some elpa modules.

---
+++ elpa/anything-sage-20141005.413/anything-sage-autoloads.el
@@ -0,0 +1,40 @@
+;;; anything-sage-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (anything-sage-command-history anything-sage-shell-describe-object-at-point
+;;;;;;  anything-sage-shell) "anything-sage" "anything-sage.el" (22090
+;;;;;;  43456 175411 399000))
+;;; Generated autoloads from anything-sage.el
+
+(autoload 'anything-sage-shell "anything-sage" "\
+
+
+\(fn)" t nil)
+
+(autoload 'anything-sage-shell-describe-object-at-point "anything-sage" "\
+
+
+\(fn)" t nil)
+
+(autoload 'anything-sage-command-history "anything-sage" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("anything-sage-pkg.el") (22090 43456 241726
+;;;;;;  538000))
+
+;;;***
+
+(provide 'anything-sage-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; anything-sage-autoloads.el ends here
---
+++ elpa/anything-sage-20141005.413/anything-sage-pkg.el
@@ -0,0 +1,1 @@
+(define-package "anything-sage" "20141005.413" "An anything extension for sage-shell-mode." (quote ((cl-lib "0.5") (anything "1.3.9") (sage-shell-mode "0.0.1"))))
